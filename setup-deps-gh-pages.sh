875f4cb73ec8f5c5bccd0bbc3be85995225e519b
Gabrielc1925 <gabrielc1925@gmail.com>
2024-07-17 22:02:24 -0600

    changed apt to apt-get as apt is not meant for use in scripting.

--- setup-deps-gh-pages.sh
+++ setup-deps-gh-pages.sh
@@ -2,22 +2,22 @@
 set -eu -o pipefail
 
 # Add Docker's official GPG key:
-sudo apt update
-sudo apt install ca-certificates curl
+sudo apt-get update
+sudo apt-get install ca-certificates curl
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
 
-# Add the repository to Apt sources:
+# Add the repository to apt-get sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt update
+sudo apt-get update
 
 # Install necessary dependencies
-sudo apt update
-sudo apt install -y git-all nginx docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo apt-get update
+sudo apt-get install -y git-all nginx docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # Configure Nginx to start on boot using systemd
 sudo systemctl enable nginx
