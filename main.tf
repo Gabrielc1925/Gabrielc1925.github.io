286083319bf26abaa94ce652f7483c774ddc6ef8
Gabrielc1925 <gabrielc1925@gmail.com>
2024-07-23 08:24:06 -0600

    Fixed login configuration for aws and hcp. Added .id identifier to end of several resourses

--- main.tf
+++ main.tf
@@ -18,6 +18,11 @@ terraform {
     }
 }
 
+provider "hcp" {
+#    project_id = "b1bbb80d-e6cd-49c6-b855-bec80721fb28"
+#    credential_file = "/home/gabrielc1925/.terraform.d/credentials.tfrc.json"
+}
+
 # Create local variable and get artifact ID from the base artifact
 data "hcp_packer_artifact" "Gabrielc1925-github-io" {
   bucket_name   = "Gabrielc1925-github-io"
@@ -28,6 +33,12 @@ data "hcp_packer_artifact" "Gabrielc1925-github-io" {
 
 provider "aws" {
     region  =   "us-east-1"
+    # profile = "PowerUserAccess-978076141947"
+    # # The following two are used for local testing
+    # shared_config_files = ["/home/gabrielc1925/.aws/config"]
+    # shared_credentials_files = ["/home/gabrielc1925/.aws/credentials"]
+    # The following two are for using with github actions saved credential secrets
+
 }
 
 resource "aws_vpc" "gh_pages_backup_site" {
@@ -40,7 +51,7 @@ resource "aws_vpc" "gh_pages_backup_site" {
 }
 
 resource "aws_subnet" "gh_pages_public_subnet" {
-    vpc_id = aws_vpc.gh_pages_backup_site
+    vpc_id = aws_vpc.gh_pages_backup_site.id
     cidr_block = "10.0.0.0/26"
     tags = {
         Name = "gh_pages_public_vpc"
@@ -48,17 +59,17 @@ resource "aws_subnet" "gh_pages_public_subnet" {
 }
 
 resource "aws_network_interface" "gh_pages_network_interface" {
-    subnet_id = aws_subnet.gh_pages_public_subnet
+    subnet_id = aws_subnet.gh_pages_public_subnet.id
     tags = {
         Name = "gh_pages_public_network_interface"
     }
 }
 
 resource "aws_instance" "gh-pages_backup" {
-    ami = data.hcp_packer_artifact.Gabrielc1925-github-io
+    ami = data.hcp_packer_artifact.Gabrielc1925-github-io.id
     instance_type = "t2.micro"
     network_interface {
-        network_interface_id = aws_network_interface.gh_pages_network_interface
+        network_interface_id = aws_network_interface.gh_pages_network_interface.id
         device_index = 0
     }
     tags = {
